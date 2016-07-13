54b291c7414358f9258b586283d9fc07226b6a90
narekcat <narekcat@gmail.com>
2016-07-13 17:45:04 +0400

    Add new views, fix bug with Auth controller file name

--- application/config/ci_my_admin.php
+++ application/config/ci_my_admin.php
@@ -3,4 +3,4 @@
 $config['ci_my_admin_template_dir'] = "";
 $config['ci_my_admin_template_dir_public'] = $config['ci_my_admin_template_dir'] . "public/themes/default/";
 $config['ci_my_admin_template_dir_admin'] = $config['ci_my_admin_template_dir'] . "admin/themes/default/";
-$config['ci_my_admin_template_dir_welcome'] = $config['ci_my_admin_temaplte_dir'] . "welcome/themes/default/";
+$config['ci_my_admin_template_dir_welcome'] = $config['ci_my_admin_template_dir'] . "welcome/themes/default/";
